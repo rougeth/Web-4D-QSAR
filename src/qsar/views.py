9161115ce3fc49c7f677cda331874a2db0e83713
Marco Rougeth <marco@rougeth.com>
2014-04-12 11:52:33 -0300

    Example of file upload

--- src/qsar/views.py
+++ src/qsar/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from qsar import app
+from qsar.forms import GromacsFileUpload
 
 @app.route('/')
 def index():
@@ -7,4 +8,18 @@ def index():
 
 @app.route('/gromacs-file-test')
 def gromacs():
-    return render_template('gromacs_file_test.html')
+    gromacs_form = GromacsFileUpload();
+
+    return render_template('gromacs_file_test.html',
+        form = gromacs_form)
+
+@app.route('/gromacs-file-test/upload', methods=['POST'])
+def gromacs_upload():
+    gromacs_form = GromacsFileUpload();
+
+    if gromacs_form.validate_on_submit():
+        return render_template('gromacs_file_test_upload.html',
+            success=True)
+
+    return render_template('gromacs_file_test_upload.html',
+        success=False)
