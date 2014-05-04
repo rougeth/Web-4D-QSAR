766709d1da3fe5ed0ad1312cf674db12b03487b8
Marco Rougeth <marco@rougeth.com>
2014-05-04 20:13:13 -0300

    Update gromacs script

--- src/scripts/preparing_for_gromacs.py
+++ src/scripts/preparing_for_gromacs.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from utils import remove_line
+from utils import remove_line, replace_line
 
 # script: preparing files to molecular dynamics
 #
@@ -8,4 +8,9 @@ from utils import remove_line
 
 
 # Removing the line '#include "ffusernb.itp"' from the file ff_file_name.itp
-remove_line('#include "ffusernb.itp"', '/tmp/ff_file')
+remove_line('#include "ffusernb.itp"', '/tmp/itp_file')
+
+# Replacing '#include "gaff_spce.itp"' to '#include "gaff tip3p.itp"' from the
+# file file_name.top
+replace_line('#include "gaff_spce.itp"', '#include "gaff tip3p.itp"',
+             '/tmp/top_file')
--- test_qsar.py
+++ test_qsar.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from src.scripts.utils import remove_line
+from src.scripts.utils import remove_line, replace_line
 
 
 class TestUtils(unittest.TestCase):
@@ -17,7 +17,6 @@ class TestUtils(unittest.TestCase):
         line = '#include "ffusernb.itp"'
         file = '/tmp/qsar_test.txt'
 
-        # Check if `line` is in `file`
         with open(file, 'w') as f:
             f.write('{}\n'.format(line))
 
@@ -29,6 +28,25 @@ class TestUtils(unittest.TestCase):
             lines = f.readlines()
 
         self.assertFalse(line in lines)
+
+    def replace_line(self):
+        line1 = '#include "gaff_spce.itp"'
+        line2 = '#include "gaff_tip3p.itp"'
+        file = '/tmp/qsar_test.txt'
+
+        with open(file, 'w') as f:
+            f.write('{}\n'.format(line1))
+
+        # Replace `line1` to `line2`
+        replace_line(line1, line2, file)
+
+        # And check it
+        with open(file, 'r') as f:
+            lines = f.readlines()
+
+        self.assertFalse(line1 in lines)
+        self.assertTrue(line2 in lines)
+
 
 
 if __name__ == '__main__':
