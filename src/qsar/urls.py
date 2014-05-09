0ffde59dd3c1f5ac839c3270f02d6ef971479fe8
Marco Rougeth <marco@rougeth.com>
2014-05-09 01:51:40 -0300

    Creating Dynamic model/form/view

--- src/qsar/urls.py
+++ src/qsar/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'app.views.home', name='home'),
+    url(r'^gromacs$', 'app.views.gromacs', name='home'),
+
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
