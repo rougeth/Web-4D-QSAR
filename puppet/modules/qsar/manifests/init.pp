c8abcf134eec7e487003374a063d54a91d1fcc26
Marco Rougeth <marco@rougeth.com>
2014-05-16 19:55:47 -0300

    Update Puppet

---
+++ puppet/modules/qsar/manifests/init.pp
@@ -0,0 +1,26 @@
+class qsar {
+	package { 'build-essential':
+	    ensure => installed,
+	}
+
+	package { 'git':
+		ensure => installed,
+	}
+
+	package { 'python-pip':
+		ensure => installed,
+	}
+
+	package { 'virtualenvwrapper':
+		ensure => installed,
+		provider => pip,
+		require => Package['python-pip'],
+	}
+
+	# links virtualenvwrapper to load automatically
+	file { '/etc/bash_completion.d/virtualenvwrapper.sh':
+	    ensure => link,
+	    target => '/usr/local/bin/virtualenvwrapper.sh',
+		require => Package['virtualenvwrapper'],
+	}
+}
