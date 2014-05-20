fefeff5501fdf98062857ce95dd7e91003430d2d
Marco Rougeth <marco@rougeth.com>
2014-05-20 19:30:22 -0300

    Update main task

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -5,34 +5,44 @@ from time import sleep
 from django.conf import settings
 from celery import task
 
-from app.models import Dynamic, Molecule
+from core.models import Dynamic, Molecule
+from core import utils
+
+
+@task
+def test_file():
+    with open('/qsar/src/files/dynamics/marco@rougeth.com/16/neutra.mol2_process/ffneutra.itp', 'r') as f:
+        pass
 
 
 @task
 def main(dynamic):
 
-    # 1: Create a folder ('<id>_molecule_process') for each .mol file. It will
-    # be use to save the output of the others commands.
+    # 1: Create a folder ('<filename>_process') for each .mol file. It will be
+    # use to save the output of the others commands.
     #
-    # 2: Copy all static files from QSAR_STATIC_FILES to <id>_molecule_process
+    # 2: Copy all static files from QSAR_STATIC_FILES to <filename>_process
     #
-    # 3: Copy all the qsar static files for each <id>_process folder.
+    # 3: Copy all the ch <id>_process folder.
 
     BASE_DIR = getattr(settings, 'BASE_DIR')
     QSAR_STATIC_FILES = getattr(settings, 'QSAR_STATIC_FILES')
     molecules = Molecule.objects.filter(dynamic=dynamic)
 
-    # Pepraring files
     for molecule in molecules:
 
-        # ../../../<id>_molecule.mol2 to <id>_molecule.mol2
         molecule_filename = molecule.file.path.split('/')[-1]
+        molecule_clean_filename = molecule_filename[:-5]
         process_path = '{}/{}_process'.format(
             # ../files/dynamics/<email>/<id>/
             os.path.dirname(molecule.file.path),
             # <id> from <id>_molecule.mol2
-            molecule_filename.split('_')[0]
+            molecule_filename
         )
+
+
+        # Pepraring files
+
         os.mkdir(process_path)
 
         os.system('cp {} {}/{}'.format(
@@ -47,21 +57,39 @@ def main(dynamic):
             process_path
         ))
 
-    # Topolbuild
-    for molecule in molecules:
-        
-        molecule_filename = molecule.file.path.split('/')[-1]
+
+        # Topolbuild
 
         topolbuild_path = '/opt/topolbuild1_2_1'
-        process_path = '{}/{}_process'.format(
-            os.path.dirname(molecule.file.path),
-            molecule_filename.split('_')[0]
-        )
 
         subprocess.Popen([
             '/usr/local/bin/topolbuild',
             '-n', '{}'.format(molecule.file.path[:-5]),
             '-dir', '{}'.format(topolbuild_path),
-            '-ff', 'gaff'], cwd=process_path)
+            '-ff', 'gaff'], cwd=process_path).wait()
+
+
+        # Preparing files for gromacs
+
+        utils.remove_line(
+            '#include "ffusernb.itp"',
+            '{}/ff{}.itp'.format(process_path, molecule_clean_filename))
+
+        utils.replace_line('#include "gaff_spce.itp"', '#include "gaff tip3p.itp"',
+             '{}/{}.top'.format(process_path, molecule_clean_filename))
+
+        os.system('cat {0}/ion_water.itp >> {0}/ff{1}nb.itp'.format(
+            process_path, molecule_clean_filename))
+
+        os.system('mv {0}/{1}.top {0}/lig.top'.format(process_path, molecule_clean_filename))
+        os.system('mv {0}/{1}.gro {0}/lig.gro'.format(process_path, molecule_clean_filename))
+
+        subprocess.Popen([
+            '/usr/local/gromacs/bin/editconf',
+            '-bt', 'cubic',
+            '-f', 'lig.gro',
+            '-o', 'lig_box.gro',
+            '-d', str(molecule.dynamic.box_size)], cwd=process_path)
+
 
     return molecules
