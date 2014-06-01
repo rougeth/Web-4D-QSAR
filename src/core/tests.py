c7681136513455ae528064069dc7f10435fcbadf
Marco Rougeth <marco@rougeth.com>
2014-06-01 18:27:21 -0300

    Update home views

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        ''' GET / must return status code 200
+        '''
+        self.assertEqual(200, self.response.status_code)
+
+    def test_used_template(self):
+        ''' GET / must use template index.html
+        '''
+        self.assertTemplateUsed('core/home.html')
