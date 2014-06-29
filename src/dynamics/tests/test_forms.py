798823dd90f33eedbf87b8b00a290e2aeb9066b7
Marco Rougeth <marco@rougeth.com>
2014-06-29 16:54:51 -0300

    Updating dynamic forms tests

--- src/dynamics/tests/test_forms.py
+++ src/dynamics/tests/test_forms.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
+from django.test.client import Client
 
 from dynamics.forms import DynamicForm, MoleculeForm
 
 
 class DynamicFormTest(TestCase):
-    def test_has_fields(self):
+    def test_dynamic_form_fields(self):
         ''' Form must have 2 fields
         '''
         form = DynamicForm()
@@ -14,9 +15,29 @@ class DynamicFormTest(TestCase):
             # assertIemsEqual is no longer supported in Python 3.2+
             self.assertItemsEqual(['email', 'box_size'], form.fields)
 
+    def test_valid_form(self):
+        ''' Form must be valid
+        '''
+        data = {
+            'email': 'marco@rougeth.com',
+            'box_size': 10,
+        }
+
+        form = DynamicForm(data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        ''' Form must be invalid
+        '''
+        data = {}
+
+        form = DynamicForm(data)
+        self.assertFalse(form.is_valid())
+
 
 class MoleculeFormTest(TestCase):
-    def test_has_fields(self):
+
+    def test_molecule_form_fields(self):
         ''' Form must have 1 fields
         '''
         form = MoleculeForm()
@@ -25,3 +46,12 @@ class MoleculeFormTest(TestCase):
         except AssertionError:
             # assertIemsEqual is no longer supported in Python 3.2+
             self.assertItemsEqual(['file'], form.fields)
+
+    def test_invalid_form(self):
+        ''' Form must be invalid
+        '''
+        data = {}
+        form = MoleculeForm(data)
+
+        self.assertFalse(form.is_valid())
+
